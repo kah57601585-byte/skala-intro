530210dc9884174f4850230d08434944af2435a8
Jongmoon Park <kah57601585@gmail.com>
2026-08-03 11:13:48 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+
+<script setup>
+
+import refexample from './refexample.vue';
+
+</script>
+
+<template>
+  <div>
+
+    <refexample />
+
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/WeatherMockdownjm.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {ref} from 'vue';
+
+const weatherList = ref([
+    { id: 'city_01', name: '서울', temp: 28, status: '맑음' },
+    { id: 'city_02', name: '서울', temp: 28, status: '비' },
+    { id: 'city_03', name: '부산', temp: 26, status: '구름' },
+])
+
+</script>
+
+<template>
+<h1>과제 1: 날씨(Mockup)</h1>
+<hr>
+
+
+
+</template>
